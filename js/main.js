85e9a2de31e4eb5a90d156c95c3684024cce5e67
Chris Mosier <chrismosier13@gmail.com>
2017-09-02 18:00:40 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+ cardsInPlay.push(cardOne);
+ cardsInPlay.push(cardTwo);
+
+
+ console.log("User flipped queen");
+ console.log("User flipped king");
+
+ console.log(cardsInPlay);
+
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again");
+	}
+};
